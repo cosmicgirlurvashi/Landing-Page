1d2c4c70c50257f6fa697bff36720884a48107c8
Urvashi Sahu <sahup9002@gmail.com>
2025-03-02 10:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for interactivity (optional)
+document.querySelectorAll('.btn-outline-primary').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Added to cart!');
+    });
+});
